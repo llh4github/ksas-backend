ff1a59ae8ace7fc6654d16648f360b88a981f42a
llh4github <lilinhong_coding@foxmail.com>
2025-03-24 16:57:34 +0800

    feat: implement user pagination and query specification

--- src/main/kotlin/api/UserApi.kt
+++ src/main/kotlin/api/UserApi.kt
@@ -1,16 +1,16 @@
 package io.github.llh4github.ksas.api
 
+import io.github.llh4github.ksas.common.req.JsonWrapper
+import io.github.llh4github.ksas.common.req.PageResult
+import io.github.llh4github.ksas.dbmodel.auth.dto.UserBaseView
 import io.github.llh4github.ksas.dbmodel.auth.dto.UserPageEle
+import io.github.llh4github.ksas.dbmodel.auth.dto.UserQuerySpec
 import io.github.llh4github.ksas.service.auth.UserService
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
 
 @Tag(name = "用户管理接口")
 @RestController
@@ -28,15 +28,12 @@ class UserApi(
         return dto
     }
 
-    /**
-     * 接口响应速度差不多
-     */
-    @GetMapping("async")
-    @Operation(summary = "根据ID获取用户(async)")
-    suspend fun getByIdAsync(@RequestParam(value = "id", required = true) id: Long) =
-        withContext(Dispatchers.IO) {
-            val dto = userService.getByIdAsync(UserPageEle::class, id)
-            logger.debug { "getByIdAsync $dto" }
-            dto
-        }
+    @PostMapping("page")
+    @Operation(summary = "分页查询")
+    fun page(
+        @RequestBody @Validated query: UserQuerySpec
+    ): JsonWrapper<PageResult<UserBaseView>> {
+        val rs = userService.pageQuery(UserBaseView::class, query, query.pageParam)
+        return JsonWrapper.ok(rs)
+    }
 }
