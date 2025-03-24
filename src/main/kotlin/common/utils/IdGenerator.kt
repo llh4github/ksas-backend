bb542b84ff96a2ed5823c609305c50c9d2b52f3e
llh4github <lilinhong_coding@foxmail.com>
2025-03-24 10:16:55 +0800

    feat: implement IdGenerator and configuration properties for ID generation and JWT

--- src/main/kotlin/common/utils/IdGenerator.kt
+++ src/main/kotlin/common/utils/IdGenerator.kt
@@ -2,36 +2,21 @@ package io.github.llh4github.ksas.common.utils
 
 import com.github.yitter.contract.IdGeneratorOptions
 import com.github.yitter.idgen.YitIdHelper
+import io.github.llh4github.ksas.config.property.IdGeneratorProperty
+import jakarta.annotation.PostConstruct
+import org.springframework.stereotype.Component
 
-object IdGenerator : LongIdGenerator {
+@Component
+class IdGenerator(private val property: IdGeneratorProperty) {
 
-    override fun nextId(): Long = YitIdHelper.nextId()
-
-    fun configGenerator(property: IdGeneratorProperty) {
+    @PostConstruct
+    fun postConstruct() {
         val options = IdGeneratorOptions(property.workerId)
         options.SeqBitLength = property.seqBitLength
         YitIdHelper.setIdGenerator(options)
     }
-}
 
-interface LongIdGenerator {
-    fun nextId(): Long
+    fun nextId(): Long = YitIdHelper.nextId()
 
     fun nextIdStr(): String = nextId().toString()
-}
-
-open class IdGeneratorProperty {
-    /**
-     *  WorkerId 最大值为2^6-1，即默认最多支持64个节点。
-     *
-     *  **多实例部署时最好修改为不同值。**
-     */
-    var workerId: Short = 1
-
-    /**
-     * **一般不用改这个配置。**
-     *
-     * 默认值6，限制每毫秒生成的ID个数。若生成速度超过5万个/秒，建议加大 SeqBitLength 到 10。
-     */
-    var seqBitLength: Byte = 6
 }
