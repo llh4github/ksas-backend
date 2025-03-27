3771b870222a9f4214b541b7014d8de4441a7635
llh4github <lilinhong_coding@foxmail.com>
2025-03-27 15:44:50 +0800

    feat: update authentication endpoint and modify anonymous URL configuration

--- src/main/kotlin/api/auth/LoginApi.kt
+++ src/main/kotlin/api/auth/LoginApi.kt
@@ -10,12 +10,10 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "认证接口")
 @RestController
-@RequestMapping("auth")
 class LoginApi(
     private val loginService: LoginService
 ) {
