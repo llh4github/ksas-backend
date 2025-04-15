b3950b93a34b18f035948a02a86bf8005864896f
llh4github <lilinhong_coding@foxmail.com>
2025-04-15 22:12:34 +0800

    feat: 添加用户权限代码获取功能，更新JWT返回结果

--- src/main/kotlin/service/auth/impl/LoginServiceImpl.kt
+++ src/main/kotlin/service/auth/impl/LoginServiceImpl.kt
@@ -3,6 +3,7 @@ package io.github.llh4github.ksas.service.auth.impl
 import io.github.llh4github.ksas.common.exceptions.UserModuleException
 import io.github.llh4github.ksas.config.property.JwtType
 import io.github.llh4github.ksas.dbmodel.auth.User
+import io.github.llh4github.ksas.dbmodel.auth.dto.UserPermissionCodeView
 import io.github.llh4github.ksas.dbmodel.auth.username
 import io.github.llh4github.ksas.payload.login.LoginOkToken
 import io.github.llh4github.ksas.payload.login.LogoutView
@@ -28,7 +29,8 @@ class LoginServiceImpl(
         if (!passwordEncoder.matches(view.password, user.password)) {
             throw UserModuleException.loginFailed(LOGIN_FAIL_MSG)
         }
-        return createJwtResult(user)
+        val permissions = fetchPermissionCodes(view.username)
+        return createJwtResult(user, permissions)
     }
 
     override fun logout(view: LogoutView) {
@@ -43,10 +45,11 @@ class LoginServiceImpl(
         val user = fetchUser(username)
         jwtService.removeToken(view.accessToken)
         jwtService.removeToken(view.refreshToken)
-        return createJwtResult(user)
+        val permissions = fetchPermissionCodes(username)
+        return createJwtResult(user, permissions)
     }
 
-    fun createJwtResult(user: User): LoginOkToken {
+    fun createJwtResult(user: User, permissions: List<String> = emptyList()): LoginOkToken {
         val access = jwtService.createToken(user.id, user.username, JwtType.ACCESS)
         val refresh = jwtService.createToken(user.id, user.username, JwtType.REFRESH).first
         return LoginOkToken(
@@ -55,6 +58,7 @@ class LoginServiceImpl(
             accessToken = access.first,
             refreshToken = refresh,
             expire = access.second,
+            permissions = permissions
         )
     }
 
@@ -63,6 +67,14 @@ class LoginServiceImpl(
             where(table.username eq username)
             select(table)
         }.fetchOneOrNull() ?: throw UserModuleException.loginFailed(LOGIN_FAIL_MSG)
+    }
+
+    fun fetchPermissionCodes(username: String): List<String> {
+        return sqlClient.createQuery(User::class) {
+            where(table.username eq username)
+            select(table.fetch(UserPermissionCodeView::class))
+        }.fetchOneOrNull()?.roles?.flatMap { it.permissions }?.map { it.code }
+            ?: throw UserModuleException.loginFailed(LOGIN_FAIL_MSG)
     }
 }
 
