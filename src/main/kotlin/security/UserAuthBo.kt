738dc0d463adc1faf285944ed37af60a4d813614
llh4github <lilinhong_coding@foxmail.com>
2025-04-16 17:31:04 +0800

    feat: 更新权限检查服务，使用UsernamePasswordAuthenticationToken替代AbstractAuthenticationToken，并添加日志记录

--- src/main/kotlin/security/UserAuthBo.kt
+++ src/main/kotlin/security/UserAuthBo.kt
@@ -1,6 +1,6 @@
 package io.github.llh4github.ksas.security
 
-import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 /**
@@ -10,16 +10,6 @@ data class UserAuthBo(
     val userId: Long,
     val username: String,
     val permissions: List<String>,
-) : AbstractAuthenticationToken(permissions.map { SimpleGrantedAuthority(it) }) {
+) : UsernamePasswordAuthenticationToken(username, null, permissions.map { SimpleGrantedAuthority(it) }) {
 
-    /**
-     * 获取凭证。这里返回空字符串，因为密码不应该被存储在内存中
-     */
-    override fun getCredentials(): Any? {
-        return ""
-    }
-
-    override fun getPrincipal(): Any {
-        return username
-    }
 }
