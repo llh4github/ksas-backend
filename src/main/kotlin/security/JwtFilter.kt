2b5e7fe28c726035871406f85735016a5fa652bb
llh4github <lilinhong_coding@foxmail.com>
2025-04-17 17:00:14 +0800

    feat: implement UserActivityService for tracking user activity and integrate with JwtFilter

--- src/main/kotlin/security/JwtFilter.kt
+++ src/main/kotlin/security/JwtFilter.kt
@@ -2,6 +2,7 @@ package io.github.llh4github.ksas.security
 
 import io.github.llh4github.ksas.config.property.AuthProperty
 import io.github.llh4github.ksas.service.extra.JwtService
+import io.github.llh4github.ksas.service.extra.UserActivityService
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -13,6 +14,7 @@ import org.springframework.web.filter.OncePerRequestFilter
 class JwtFilter(
     private val property: AuthProperty,
     private val jwtService: JwtService,
+    private val userActivityService: UserActivityService
 ) : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
@@ -22,6 +24,7 @@ class JwtFilter(
         extractJwt(request)?.let {
             jwtService.validateTokenConvertBo(it)?.let {
                 SecurityContextHolder.getContext().authentication = it
+                userActivityService.recordUserActivity(it.userId)
             }
         }
         filterChain.doFilter(request, response)
