e267c82f711d05c66e2ca74610b2980a58174477
llh4github <lilinhong_coding@foxmail.com>
2025-04-21 16:58:12 +0800

    feat: configure Redis cache settings and add Spring Boot cache starter dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
     runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
